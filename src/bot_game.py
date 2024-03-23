6d118ea59b0986ec4b35734261d2e002d3faab3d
MatteoC <matteocamazzola14@gmail.com>
2024-03-23 19:04:30 -0400

    .

--- src/bot_game.py
+++ src/bot_game.py
@@ -13,7 +13,6 @@ selected_piece = None
 square_size = 59
 gameBoard = Board()
 
-
 def draw_chessboard():
     square_color = "#F5F5DC"
     for row in range(8):
@@ -30,20 +29,35 @@ def draw_chessboard():
 def place_pieces():
     # Delete all previously drawn pieces
     canvas.delete("piece")
-    for row in range(8):
-        for col in range(8):
-            piece = gameBoard.chessBoard[row][col]
-            if piece is not None:
-                # Create a new image for the piece with the tag "piece"
-                canvas.create_image(col * 59 + 59 / 2, row * 59 + 59 / 2, image=piece.image,
-                                    anchor="center", tags="piece")
-
+    if current_player == "black":
+        for row in range(8):
+            for col in range(8):
+                piece = gameBoard.chessBoard[row][col]
+                if piece is not None:
+                    # Create a new image for the piece with the tag "piece"
+                    canvas.create_image(col * 59 + 59 / 2, row * 59 + 59 / 2, image=piece.image,
+                                        anchor="center", tags="piece")
+    else:
+        #swap_board()
+        for row in range(8):
+            for col in range(8):
+                piece = gameBoard.chessBoard[row][col]
+                if piece is not None:
+                    new_row = row_col_swap(row)
+                    new_col = row_col_swap(col)
+                    # Create a new image for the piece with the tag "piece"
+                    canvas.create_image(new_col * 59 + 59 / 2, new_row * 59 + 59 / 2, image=piece.image,
+                                        anchor="center", tags="piece")
 
 def on_canvas_click(event):
     global selected_piece
     global current_player
-    col = int(event.x // square_size)
-    row = int(event.y // square_size)
+    if current_player == "white":
+        col = row_col_swap(int(event.x // square_size))
+        row = row_col_swap(int(event.y // square_size))
+    else:
+        col = int(event.x // square_size)
+        row = int(event.y // square_size)
     checkmate_check_colour = current_player
     if current_player == "black":
         checkmate_check_colour = "white"
@@ -134,6 +148,9 @@ def show_possible_moves(piece):
 
     for move in valid_moves:
         row, col = move
+        if current_player == "white":
+            row = row_col_swap(row)
+            col = row_col_swap(col)
 
         # Create an off-screen image (RGBA for transparency support)
         img = Image.new('RGBA', (square_size, square_size), (0, 0, 0, 0))  # White, opaque background
@@ -181,9 +198,45 @@ def check_for_game_over(checkmate_check_colour):
         messagebox.showinfo("Game Over", game_over_message)
         root.destroy()  # Close the Tkinter window after the user acknowledges the message
 
+def row_col_swap(to_swap):
+    if to_swap == 0:
+        return 7
+    if to_swap == 1:
+        return 6
+    if to_swap == 2:
+        return 5
+    if to_swap == 3:
+        return 4
+    if to_swap == 4:
+        return 3
+    if to_swap == 5:
+        return 2
+    if to_swap == 6:
+        return 1
+    if to_swap == 7:
+        return 0
+
+def swap_board():
+    for row in range(4):
+        for col in range(8):
+            to_swap = gameBoard.chessBoard[row][col]
+            row_to_move = row_col_swap(row)
+            col_to_move = row_col_swap(col)
+            temp = gameBoard.chessBoard[row_to_move][col_to_move]
+            gameBoard.chessBoard[row_to_move][col_to_move] = to_swap
+            gameBoard.chessBoard[row][col] = temp
+
 
 draw_chessboard()
 place_pieces()
+if current_player == "black":
+    bot_colour = "white"
+    bot_move = random_moves(gameBoard, bot_colour)
+    if bot_move is not None:
+        gameBoard.make_move(bot_move[1][0], bot_move[1][1], gameBoard.valid_moves(bot_move[0].position),
+                            bot_move[0])
+        place_pieces()
+
 
 canvas.bind("<Button-1>", on_canvas_click)
 root.mainloop()
